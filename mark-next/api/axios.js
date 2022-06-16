0000563f5a974167a770e00a245bb11973d01310
luxurystar <qhshef@gmail.com>
2022-06-17 00:45:52 +0900

    [next] - server url 추가

--- mark-next/api/axios.js
+++ mark-next/api/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { tokenSelector } from "../recoil/selectors";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:8090/api/v1/",
+  // baseURL: "http://localhost:8090/api/v1/",
+  baseURL:
+    "http://ec2-3-37-91-227.ap-northeast-2.compute.amazonaws.com:8090/api/v1/",
   timeout: 60000,
   withCredentials: false,
   headers: {
